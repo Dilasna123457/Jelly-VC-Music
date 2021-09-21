9110f081c0932554fa772e9ec5a545510eab08e0
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-21 18:34:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from os import environ
+# import logging
+from pyrogram import Client, idle
+
+API_ID = int(environ["API_ID"])
+API_HASH = environ["API_HASH"]
+SESSION_NAME = environ["SESSION_NAME"]
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc." + environ["PLUGIN"],
+        "ping",
+        "sysinfo"
+    ]
+)
+
+app = Client(SESSION_NAME, API_ID, API_HASH, plugins=plugins)
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> USERBOT STARTED by @senuinfinity')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED by @senuinfinity')
